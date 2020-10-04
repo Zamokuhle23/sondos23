4f6d196b70325362a978927e23f2a2e143ff5608
Zamokuhle23 <65773452+Zamokuhle23@users.noreply.github.com>
2020-10-04 04:30:53 +0300

    Update proj.py

--- proj.py
+++ proj.py
@@ -1,14 +1,23 @@
 import math
 
-def merge_list(list):
-  return str([str([ch for ch in word ])[1:-1] for word in list]).replace("'",'')
+def merge_list(*args):
+    b = []
+    for i in args:
+        if type(i) == str or i is None:
+            b.append(i)
+        else:
+            for j in i:
+                b.append(j)
+    return b
+print(merge_list([0,0], 'nice', {5,4,1}, (12,9), None))
 
-print(merge_list([{0, 2, 3},[12,3,4,5],[3,4,5],["Sondos"]]))
 
-def merge_to_list(list):
-  return str([str([ch for ch in word])[1:-1] for word in list ]).replace("'",'')
 
-print(merge_to_list([[0,2,3],[12,3,4,5],[3,4,5]]))
+
+def merge_to_list(args):
+    return [i for x in args for i in x]
+
+print(merge_to_list([[1,2,3],[3,4,5,6],[5,6,7]]))
 
 def multi_power(original, powers):
     return [math.pow(original[i],powers[i]) for i in range(len(original))]
